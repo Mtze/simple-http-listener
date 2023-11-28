0107f220a0b32d06cee2c19ace4385cf346b7f0a
Matthias Linhuber <matthias@linhuber.org>
2023-11-28 14:44:06 +0100

    Initial commit

---
+++ main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	log "github.com/sirupsen/logrus"
+)
+
+const (
+	port = 80
+)
+
+func logResponse(c *gin.Context) {
+	// Log the request
+	log.WithFields(log.Fields{
+		"method": c.Request.Method,
+		"path":   c.Request.URL.Path,
+		"origin": c.Request.RemoteAddr,
+	}).Info("Request received")
+
+	// Log body content
+	body, err := io.ReadAll(c.Request.Body)
+	if err != nil {
+		log.WithError(err).Info("Failed to read request body")
+	} else {
+		var out bytes.Buffer
+		json.Indent(&out, body, "", "  ")
+		fmt.Print(out.String())
+	}
+
+	// Send the response
+	c.JSON(http.StatusOK, gin.H{
+		"message": "pong",
+	})
+}
+
+func main() {
+	// Start the server
+	log.SetLevel(log.DebugLevel)
+	log.Info("Starting server...")
+
+	r := gin.Default()
+	r.POST("/", logResponse)
+	log.Info("Server started")
+
+	log.Panic(r.Run(fmt.Sprintf(":%d", port)))
+}
